68c6b5970dee8ab9b6d43621862e638a8625da12
Edwin Trigos <82897868+Ewin24@users.noreply.github.com>
2023-07-24 13:24:13 -0500

    Add files via upload

--- JS-PROYECTS/To-do/app.js
+++ JS-PROYECTS/To-do/app.js
@@ -1,41 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const tablebody = document.getElementById("table-body");
+    const tableBody = document.getElementById("table-body");
 
-  function deleteTodo(element) {
-    return element;
-  }
 
-  function addTodo() {
-    const btnAdd = document.getElementById("add");
-    const titleLabel = document.getElementById("title-label");
-    const descLabel = document.getElementById("description-label");
-
-    if (titleLabel != "" && descLabel != "") {
-      btnAdd.addEventListener("click", function (evt) {
-        let todo = `
-              <tr id="" class="">
-                  <td scope="row">${titleLabel.value}</td>
-                  <td>${descLabel.value}</td>
-                  <td>
-                  <button id="edit" class="btn btn-primary">Edit</button>
-                  <button id="delete" class="btn btn-danger">Delete</button>
-                  </td>
-              </tr>
-              `;
-      });
+    function deleteTodo(element) {
+        const btnDelete = document.getElementById("delete");
+        btnDelete.addEventListener("click", function () { console.log(btnDelete) })
     }
-  }
 
-  function editTodo(element) {
-    return element;
-  }
+    function addTodo() {
+        const btnAdd = document.getElementById("add");
+        const titleLabel = document.getElementById("title-label");
+        const descLabel = document.getElementById("description-label");
 
-  function listTodo(element) {
-    return element;
-  }
+        if (titleLabel != "" && descLabel != "") {
+            btnAdd.addEventListener("click", function (evt) {
+                let element = document.createElement('tr')
 
-  function idCalc(tablebody) {
-    tablebody.firsrChild.tr;
-    return;
-  }
+                let todoText = `
+                    <td scope="row">${titleLabel.value}</td>
+                    <td>${descLabel.value}</td>
+                    <td>
+                    <button id="edit" class="btn btn-primary">Edit</button>
+                    <button id="delete" class="btn btn-danger">Delete</button>
+                    </td>
+                `;
+                todo = element.innerHTML = todoText
+                tableBody.appendChild(todo)
+            });
+        }else{
+            console.log("Agrega una descripcion y un titulo")
+        }
+    }
+
+    function editTodo(element) {
+        const btnEdit = document.getElementById("edit");
+        btnEdit.addEventListener("click", function () { console.log(btnEdit) })
+
+    }
+
+    function listTodo(element) {
+        return element;
+    }
+
+    function idCalc(tablebody) {
+        tablebody.firsrChild.tr;
+        return;
+    }
+
+    deleteTodo()
+    editTodo()
+    addTodo()
 });
